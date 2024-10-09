95a03a2bebdd5655eb61e069873b981be3d1a93b
JorginVln <164012056+JorginVln@users.noreply.github.com>
2024-10-08 22:56:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingês é HI (RAI)'
+)
+
+criaCartao(
+    'Voleibol',
+    'Quais os principais fundamentos do Voleibol?',
+    'Os fundamentos básicos do voleibol são cinco: saque, ataque, bloqueio, levantamento e recepção.'
+)
+
+criaCartao(
+    'Voleibol',
+    'Quais os tipos de Voleibol?',
+    'Vôlei de Quadra, Vôlei de Praia e Vôlei Sentado'
+)
